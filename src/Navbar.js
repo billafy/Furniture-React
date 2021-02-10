bfcbbaf848def09c70ef3312e59c8ad993780a18
devadula-nandan <devadula.nandan@gmail.com>
2021-02-10 11:45:10 +0530

    fixed cart

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './utils/navbar.css';
-import {navlinks} from './utils/constants';
-import {FaShoppingCart} from 'react-icons/fa';
+import { navlinks } from './utils/constants';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Navbar = () => {
 	return (
 		<nav>
 			<div className="home">
 				<Link to='/'>
-				<img className="homeimg" 
-					 src="https://github.com/billafy/furniture-react/blob/main/components/home1.png?raw=true" 
-					 alt="home"/>
+					<img className="homeimg"
+						src="https://github.com/billafy/furniture-react/blob/main/components/home1.png?raw=true"
+						alt="home" />
 				</Link>
 			</div>
 			<div className="left">
 				<ul>
 					{navlinks.map((link) => {
-							return (
-								<Link to={link.to} key={link.id}><li>{link.title}</li></Link>	
-							);
-						})}
+						return (
+							<Link to={link.to} key={link.id}><li>{link.title}</li></Link>
+						);
+					})}
 				</ul>
 			</div>
 			<div className="right">
-				<Link to='/cart'><p><FaShoppingCart/></p></Link>
+				<Link to='/cart'><span className="carticon"><FaShoppingCart /></span></Link>
 			</div>
 			<div className="burger">
 				<div className="all-lines">
@@ -33,7 +33,7 @@ const Navbar = () => {
 					<div className="line"></div>
 				</div>
 			</div>
-		</nav>	
+		</nav>
 	);
 }
 
